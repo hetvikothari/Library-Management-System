a25c1d5c79e0c3b2ba3f13516af0347ebefdeace
hetvikothari <hetvi.kothari@somaiya.edu>
2022-01-04 00:09:00 +0530

    Migrated to Postgres Database for Heroku

--- library/__init__.py
+++ library/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba156'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+ENV = 'dev'
+if ENV == 'dev':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
